1100a8b5a48667c7639299daf7655a44d6620933
wychengnuo <346319096@qq.com>
2015-01-08 19:31:44 +0800

    发起项目-编辑项目

--- web/crowdfunding/js/animation.js
+++ web/crowdfunding/js/animation.js
@@ -8,3 +8,29 @@ function numberScroll(className, number, delay) {
         $(e).html(number);
     }, delay);
 }
+
+$(function(){
+    $('#indexslider').slides({
+        effect: 'fade, fade',
+        crossfade: true,
+        container:'index-img',
+        paginationClass: 'index-pagination',
+        preloadImage: '',
+        play:3000,
+        fadeSpeed: 500,
+        pause:10,
+        generatePagination: false,
+        animationStart: function(current){
+            $('.index-pagination span').stop().animate({width:0}).hide();
+        }	,
+        animationComplete: function(current){
+            var $back=$('.back');
+            var leftw=(current-1)*34;
+            $back.animate({left:leftw},1000,'easeOutBack');
+            $('.index-pagination li').eq(current-1).find('span').show().animate({width:60},3000);
+        },
+        slidesLoaded: function() {
+            $('.index-pagination li').eq(0).find('span').animate({width:60},3000);
+        }
+    });
+});
