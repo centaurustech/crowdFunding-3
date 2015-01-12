a4d9fd92b10d49175e998efb9da8850587dcc303
wychengnuo <346319096@qq.com>
2015-01-12 18:37:19 +0800

    空间动态

--- web/crowdfunding/js/pop.js
+++ web/crowdfunding/js/pop.js
@@ -1,21 +1,26 @@
 $(function(){
 
-    $(".head_pop").hide();
-    var width = $('#in_head').offset().left;
-    $('.head_pop').css('margin-left', width);
-    // 鼠标停在头像上
-    $("#in_head").mouseover(function(){
-        $(".head_pop").slideDown(500);
-    });
-    // 鼠标离开头像
-    $("#in_head").mouseleave(function(){
-        $(".head_pop").slideUp(500);
-    });
+    var width = $('#login').offset().left;
 
+    $(".head_div").css('left', width + 260);
+    $(".head_div").css('height', '65px');
+
+    $('#login').remove();
+    $(".head_pop").hide();
+
+    // 鼠标停在头像上
+    $(".head_div").mouseenter(function(){
+        $(".head_pop").slideDown(500);
+        $(".head_div").css('height', '266px');
+    });
+    $(".head_div").mouseleave(function(){
+        $(".head_pop").slideUp(500);
+        $(".head_div").css('height', '65px');
+    });
 
     // 鼠标停在头像上
     $("#head_detail").hide();
-    $("#pro_head").mouseover(function(){
+    $("#pro_head").mouseenter(function(){
         $("#head_detail").show();
         $(".pro_context_body").hide();
     });
